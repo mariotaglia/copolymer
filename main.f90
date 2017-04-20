611226709001a705da167493898e22d88c9b028c
gerva <gzaldivar@qi.fcen.uba.ar>
2017-04-20 14:58:40 -0300

    fixed ier in fkfun for rank ne zero and commented lnqfilename

--- main.f90
+++ main.f90
@@ -66,7 +66,7 @@ real*8 sumpi,sumrho,sumrhopol, sumrho2, sumrho2mol !suma de la fraccion de polim
 ! single layer files
 character*18 sysfilename      ! contains value of free energy, input parameter etc
 character*26 denssolfilename  ! contains the denisty of the solvent
-character*27 lnqfilename  ! contains the denisty of the solvent
+!character*27 lnqfilename  ! contains the denisty of the solvent
 character*28 densendfilename
 CHARACTER*24 totalfilename
 CHARACTER*24 xtotalfilename
@@ -368,7 +368,7 @@ write(totalfilename,'(A13,BZ,I3.3,A1,I3.3,A4)')'densitytotal.',countfileuno,'.',
 write(xtotalfilename,'(A13,BZ,I3.3,A1,I3.3,A4)')'xdensitytota.',countfileuno,'.',countfile,'.dat'
 
 write(denspol1filename,'(A16,BZ,I3.3,A1,I3.3,A4)')'densitypolymer1.',countfileuno,'.',countfile,'.dat'
-write(lnqfilename,'(A16,BZ,I3.3,A1,I3.3,A4)')'chemical_potent.',countfileuno,'.',countfile,'.dat'
+!write(lnqfilename,'(A16,BZ,I3.3,A1,I3.3,A4)')'chemical_potent.',countfileuno,'.',countfile,'.dat'
 
 open(unit=310,file=sysfilename)
 open(unit=321,file=denspol1filename)
@@ -376,7 +376,7 @@ open(unit=322,file=denspol2filename)
 open(unit=323,file=totalfilename)
 open(unit=325,file=xtotalfilename)
 open(unit=330,file=denssolfilename)
-open(unit=324,file=lnqfilename)
+!open(unit=324,file=lnqfilename)
 
 do i=1,n
 write(321,*)zc(i),avpol(i,1)
@@ -384,7 +384,7 @@ write(322,*)zc(i),avpol(i,2)
 avtmp = avpol(i,2)+avpol(i,1)
 write(323,*)zc(i),avtmp
 write(325,*)zc(i),xpol(i)
-write(324,*)zc(i),dlog(xpol(i))-dlog(q(i))
+!write(324,*)zc(i),dlog(xpol(i))-dlog(q(i))
 write(330,*)zc(i),xsol(i)
 enddo
 
@@ -416,7 +416,7 @@ CLOSE(321)
 CLOSE(322)
 CLOSE(323)
 CLOSE(325)
-CLOSE(324)
+!CLOSE(324)
 close(330)
 
 countfile = countfile+1 ! next
