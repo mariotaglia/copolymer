88941cecdb079c943d354365fe1ca999d0e40571
mario <m_taglia@hotmail.com>
2018-12-13 12:17:04 -0300

    added stop if norm > error

--- main.f90
+++ main.f90
@@ -134,6 +134,7 @@ do while (actionflag.lt.3)
    if((norma.gt.error).or.(ier.lt.0).or.(isnan(norma))) then
       if(actionflag.gt.0) then
          print*, " I am ", rank, " I stopped the work"
+         stop
       endif
    endif
 
