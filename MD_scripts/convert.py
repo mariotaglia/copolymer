5e9b391787727cb53b4abbb624834873c9f958e1
mario <mario.tagliazucchi@gmail.com>
2024-08-16 08:32:32 -0300

    Created script, tutorial and example folders

--- MD_scripts/convert.py
+++
@@ -1,8 +0,0 @@
-import parmed as pmd
-
-#convert prmtop and inpcrd into top and gro
-amber = pmd.load_file('c16k3_H.prmtop','c16k3_H.rst7')
-#
-amber.save('c16k3_H.top')
-amber.save('c16k3_H.crd')
-#
